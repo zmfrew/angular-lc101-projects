719dd897959deb5c05f0d40382b726909b33d246
Zach Frew <zachary.frew@ascension-external.org>
2020-03-26 08:55:18 -0500

    Update movie-list, chores-list, and fav-photos for part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Favorite Photos';
+  image1 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fblogs-images.forbes.com%2Fdavidhochman%2Ffiles%2F2015%2F03%2Fbreckenridge-1940x1327.jpg&f=1&nofb=1';
+  image2 = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F_83rZEgX6bQY%2FSwmPJ13XcAI%2FAAAAAAAAJgQ%2Fr2t6074rsT4%2Fs1600%2FGrayton%2BBeach.JPG&f=1&nofb=1';
+  image3 = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fsonnybou.ca%2Fssbou2013%2Ftimpanogos15.jpg&f=1&nofb=1';
 
   constructor() { }
 
